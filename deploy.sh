f8a8d80587191c47a652b7841e9ec930f8482da4
LnAlex <contact@alexandrelion.com>
2016-05-16 18:37:21 +0200

    Change deployement to s3 bucket

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,13 @@
 
 echo Uploading...
 
-ncftp <<EOF
-open -u $FTP_USER -p $FTP_PASS $FTP_HOST
-cd alexandre.xyz/
-put -R *
-bye
-EOF
+#ncftp <<EOF
+#open -u $FTP_USER -p $FTP_PASS $FTP_HOST
+#cd alexandre.xyz/
+#put -R *
+#bye
+#EOF
+
+sh s3-put -k $S3_ACCESS_KEY -s $S3_SECRET_KEY -T . $S3_BUCKET
 
 echo Done !
