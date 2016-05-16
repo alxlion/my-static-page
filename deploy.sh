f8b74f5d3490bc980023769a23d5573c291d4e55
LnAlex <contact@alexandrelion.com>
2016-05-16 18:56:14 +0200

    Revert "Change deployement to s3 bucket"
    This reverts commit f8a8d80587191c47a652b7841e9ec930f8482da4.

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,11 @@
 
 echo Uploading...
 
-#ncftp <<EOF
-#open -u $FTP_USER -p $FTP_PASS $FTP_HOST
-#cd alexandre.xyz/
-#put -R *
-#bye
-#EOF
-
-sh s3-put -k $S3_ACCESS_KEY -s $S3_SECRET_KEY -T . $S3_BUCKET
+ncftp <<EOF
+open -u $FTP_USER -p $FTP_PASS $FTP_HOST
+cd alexandre.xyz/
+put -R *
+bye
+EOF
 
 echo Done !
