e9e37dfcb75bf0797569d833547e2fb5383507af
lnalex <lion@alexandre.xyz>
2016-07-04 22:54:39 +0200

    Upgrade to v2

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(function(){
+  $(".love").typed({
+    strings: [
+      "open-source",
+      "free and open-source software",
+      "Vim",
+      "Linux",
+      "coffee",
+      "StackOverflow",
+      "clean code",
+      "100% test coverage",
+      "learning",
+      "leadership",
+      "Github",
+      "hamburgers",
+      "innovation",
+      "improvement",
+      "Bitcoins",
+      "reddit",
+      "music"
+    ],
+    typeSpeed: 0,
+    loop: true
+  });
+});
